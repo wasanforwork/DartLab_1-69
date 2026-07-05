91b18909d475e91b23da72b206fe403df976b7cc
wasanforwork <wasan.r@gmail.com>
2026-07-05 20:56:05 +0700

    exercise6

--- lab2_exercise6.dart
+++ lab2_exercise6.dart
@@ -1,8 +1,23 @@
-double calculateArea({required double width, required double height}) {
-  return width * height;
-}
+import 'dart:math';
+
+double calculateAreaRegtangle({
+  required double width,
+  required double height,
+}) => width * height;
+
+double calculateAreaTriangle({required double base, required double height}) =>
+    0.5 * base * height;
+
+double calculatevolumeCylinder({
+  required double radius,
+  required double height,
+}) => pi * pow(radius, 2) * height;
 
 void main() {
-  double area = calculateArea(width: 10.0, height: 5.5);
-  print('Area (10.0 x 5.5) = $area');
+  double areareg = calculateAreaRegtangle(width: 10.0, height: 5.5);
+  double areatri = calculateAreaTriangle(base: 10.0, height: 5.5);
+  double volumecy = calculatevolumeCylinder(radius: 10.0, height: 5.5);
+  print('Area Regtangle: $areareg');
+  print('Area Triangle: $areatri');
+  print('Volume Cylender: ${volumecy.toStringAsFixed(2)}');
 }
